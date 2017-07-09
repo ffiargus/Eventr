9d102113ca351be3cf64093fd3aa203c97a86e0c
ffiargus <rm.chow@outlook.com>
2017-07-09 19:11:26 -0400

    Adds templates and displays events

---
+++ rsvp_umbrella/apps/rsvp_web/web/controllers/event_controller.ex
@@ -0,0 +1,15 @@
+defmodule RsvpWeb.EventController do
+  use RsvpWeb.Web, :controller
+
+  def show(conn, %{"id" => id}) do
+    event = Rsvp.EventQueries.get_by_id(id)
+    |> IO.inspect()
+
+    render conn, "details.html", event: event
+  end
+
+  def list(conn, _params) do
+    events = Rsvp.EventQueries.get_all
+    render conn, "list.html", events: events
+  end
+end
